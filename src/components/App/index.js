9c2d3bd1ca3d0c43cc2464a3950bc87e36aa224f
Misha-Kirichenko <m.kirichenko.dev@gmail.com>
2023-11-26 22:32:41 +0400

    server-actions-added

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,17 +1,24 @@
-import './app.css'
-import ItemList from "../ItemsList";
-import Cart from '../Cart';
-
-const mockData = [
-  { id: 1, name: "chair-1", description: "Chair-1 desc", imgUrl: "https://rusticreddoor.com/cdn/shop/products/cherry-wood-dining-chair-ladder-back.jpg?v=1694125456&width=1445", price: 20 },
-  { id: 2, name: "chair-2", description: "Chair-2 desc", imgUrl: "https://wakefitdev.gumlet.io/img/npl_modified_images/darcy/sofa_WLCHRDRCFVBL/sofa_WLCHRDRCFVBL_1.jpg?w=732", price: 22 },
-  { id: 3, name: "chair-3", description: "Chair-1 desc", imgUrl: "https://www.branchfurniture.com/cdn/shop/products/B-Bmin_db723f4f-7f4e-4c74-9944-e38582290394.jpg?v=1620184069&width=1000", price: 24 },
-  { id: 4, name: "chair-4", description: "Chair-2 desc", imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3Q2sy6mS9pTVCJIXrIC882kWYiB788cOwzQ&usqp=CAU", price: 26 },
-];
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import MainPage from '../MainPage';
+import CartPage from '../CartPage';
+import './app.css';
 
 const App = () => {
-  return <><ItemList data={mockData} /><Cart /></>;
+  return <Router>
+    <Routes>
+      <Route
+        path="/"
+        exact
+        element={<MainPage />}
+      />
+      <Route
+        path="/cart"
+        exact
+        element={<CartPage />
+        }
+      />
+    </Routes>
+  </Router>
 }
 
 export default App;
--- src/components/Item/index.js
+++ src/components/Item/index.js
@@ -1,13 +1,41 @@
+import { useContext } from 'react';
+import CartContext from '../../contexts/cartContext';
 import './item.css'
 
-const Item = ({ itemData }) => {
+const Item = ({ itemData, quantityState }) => {
+  const { quantities, setQuantities } = quantityState;
+  const { setCartItems } = useContext(CartContext);
+
+  const handleSetQuantity = (e) => {
+    const { value } = e.target;
+    setQuantities(prevquantities => {
+      return {
+        ...prevquantities,
+        [itemData.id]: Number(value)
+      }
+    })
+  }
+
+  const handleAddToCart = () => {
+    setCartItems(prevItems => {
+      const newItemQuantity = prevItems[itemData.id] ? prevItems[itemData.id] + quantities[itemData.id] : quantities[itemData.id];
+      const newCartItems = {
+        ...prevItems,
+        [itemData.id]: newItemQuantity
+      }
+      localStorage.setItem("cart", JSON.stringify(newCartItems));
+      return newCartItems;
+    })
+    setQuantities(prevquantities => ({ ...prevquantities, [itemData.id]: 1 }))
+  }
+
   return (<div className="item">
-    <div className="item-image"><img src={itemData.imgUrl} className="resp-img" /></div>
+    <div className="item-image"><img src={itemData.imgUrl} className="resp-img" alt={itemData.name} /></div>
     <p><b>Name:</b>{itemData.name}</p>
     <div><b>Description:</b><p>{itemData.description}</p></div>
     <p><b>Price:</b>{itemData.price}$</p>
-    <p><input type="number" className="item-price" defaultValue={1} min={1} /></p>
-    <p><button>Add to cart</button></p>
+    <p><input onChange={handleSetQuantity} type="number" className="item-price" min={1} value={quantities[itemData.id]} /></p>
+    <p><button onClick={handleAddToCart}>Add to cart</button></p>
   </div>);
 }
 
